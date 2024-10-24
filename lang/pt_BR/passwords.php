7484175b7463ea2f5d9469c89e2c181dfb75b811
Gabriel Carlos <gabriel_webdesigner@hotmail.com>
2024-10-24 08:15:10 -0300

    use translate and implemetation logviewer

---
+++ lang/pt_BR/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Sua senha foi redefinida.',
+    'sent' => 'Nós enviamos um link de redefinição de senha por e-mail.',
+    'throttled' => 'Por favor, aguarde antes de tentar novamente.',
+    'token' => 'Este token de redefinição de senha é inválido.',
+    'user' => 'Não encontramos um usuário com este endereço de e-mail.',
+
+];
